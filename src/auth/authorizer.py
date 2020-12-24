91c57408f60f7d05f1246612c18127a214ffa427
Brandon Sturgeon <brandon@brandonsturgeon.com>
2020-12-23 18:14:23 -0800

    Initial

---
+++ src/auth/authorizer.py
@@ -0,0 +1,71 @@
+import json
+import os
+from json.decoder import JSONDecodeError
+
+def is_authorized(authorized):
+    return { "isAuthorized": authorized }
+
+def deny():
+    return is_authorized(False)
+
+def accept():
+    return is_authorized(True)
+
+def get_env(env_name):
+    auth_config = {}
+    try:
+        if auth_config_string := os.getenv("JSON_ENV"):
+            auth_config = json.loads(auth_config_string)
+    except:
+        pass
+
+    return os.getenv(env_name) or auth_config.get(env_name, None)
+
+def get_allowed_keys(route):
+    route = route.replace("/", "")
+    route = route.replace("-", "_")
+    route = route.upper()
+
+    allowed_key_names = get_env(f"{route}_ALLOWED")
+
+    if not allowed_key_names:
+        return None
+
+    allowed_keys = set()
+    allowed_key_names = allowed_key_names.split(",")
+    for key_name in allowed_key_names:
+        key = get_env(key_name)
+
+        if not key:
+            print(f"Warning! Couldn't find key in environment: {key_name}")
+            continue
+
+        allowed_keys.add(key)
+
+    return allowed_keys
+
+def handler(event, context):
+    key = event.get("headers", {}).get("authorization", None)
+
+    if key is None:
+        print("Auth is none, returning False")
+        return deny()
+
+    route = event.get("rawPath", None)
+
+    if route is None:
+        print("Route is none, returning False")
+        return deny()
+
+    allowed_keys = get_allowed_keys(route)
+
+    if allowed_keys is None:
+        print("Couldn't find any allowed keys for given route, '{}'. Denying.".format(route))
+        return deny()
+
+    is_authorized = key in allowed_keys
+
+    if is_authorized:
+        return accept()
+
+    return deny()
---
+++ src/store_record/app.py
@@ -0,0 +1,33 @@
+import base64
+import boto3
+import json
+import lzma
+import time
+
+response = {
+    "statusCode": 204,
+    "body": None
+}
+
+s3_client = boto3.client('s3')
+
+def decode_record(record):
+    b64 = base64.b64decode(record)
+    raw = lzma.decompress(b64)
+
+    return raw
+
+def lambda_handler(event, context):
+    data = event.get("body")
+
+    name = data.get("name", "unknown-name")
+    record = decode_record(data["compress"])
+    realm = data.get("realm", "unknown-realm")
+    spawn_time = data.get("spawnTime", round(time.time()))
+    owner_steam_id = data.get("owner", "unattributed").lower()
+
+    save_path = f"records/{realm}/owner_steam_id/{name}-{spawn_time}.txt"
+
+    s3_client.put_object(Body=record, Bucket="CFC-RECORD-KEEPER", Key=save_path)
+
+    return response
