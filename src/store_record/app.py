2903b5390fd93e29c052580d5cd06d4446764e45
Pierce <pierce@vulpes.dev>
2021-08-12 18:18:15 -0400

    accept record in sub table while keeping realm in top most table

--- src/store_record/app.py
+++ src/store_record/app.py
@@ -21,15 +21,17 @@ def decode_record(record):
 def lambda_handler(event, context):
     data = json.loads(event["body"])
 
-    name = data.get("name", "unknown-name").upper()
-    record = decode_record(data["compress"])
+    record = data.get("record", {})
     realm = data.get("realm", "unknown-realm")
+
+    name = data.get("name", "unknown-name").upper()
+    decoded_record_data = decode_record(data["compress"])
     spawn_time = data.get("spawnTime", round(time.time()))
     owner_steam_id = data.get("owner", "unattributed").lower()
 
     save_path = f"records/{realm}/{owner_steam_id}/{spawn_time}-{name}.txt"
     print(f"Saving {save_path}")
 
-    s3_client.put_object(Body=record, Bucket="cfc-record-keeper", Key=save_path)
+    s3_client.put_object(Body=decoded_record_data, Bucket="cfc-record-keeper", Key=save_path)
 
     return response
